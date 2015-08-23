7d76a38ed01c1d1c4bb88cc6b0e7c10d4f4ee590
carol <hcarol.wu@gmail.com>
2015-08-23 13:01:01 -0400

    rdpengAssignment2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,51 @@
-## Put comments here that give an overall description of what your
+## Caching the Mean of a vector
+
 ## functions do
 
-## Write a short comment describing this function
+## MakeVector
 
-makeCacheMatrix <- function(x = matrix()) {
 
+makecatcheMatrix <- function(x = matrix()) {
+    
+  m <- Null
+  
+   set <- function(y) {
+     
+     x <<- y
+     
+     m <<- Null
+   }
+
+   get <- function() x
+   
+   setmean <- function(mean) m <<-mean
+   
+   getmean <- function() m
+                       
+   list (set =set, get =get, 
+         setmean = setmean,
+         getmean = getmean)
+   
+   
 }
 
 
-## Write a short comment describing this function
+## calculate the mean of the special vector
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  
+  m <- x$getmean()
+  
+  if (!is.null(m)) {
+              message("getting cached data")
+              return(m)
+  }
+  data <- x$get()
+  
+  m <- mean(data, ...)
+  
+  x$setmean(m)
+  
+  m
 }
